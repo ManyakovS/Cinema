039ee8c7bbc5407b2ad7e1aa049f35a9cea0c1e9
Rafael2111 <117172668+Rafael2111@users.noreply.github.com>
2023-03-15 19:12:19 +0700

    fix

--- src/components/LanguageRegistrationPageComponents.vue
+++ src/components/LanguageRegistrationPageComponents.vue
@@ -90,7 +90,7 @@
                 
                 if(el.changedTouches[0].clientY < this.y)
                 {
-                    this.translateY -= 1.8
+                    this.translateY -= 3.6
                     document.getElementsByClassName('swiper_country__list')[0].style.transition = "transform .1s linear;"
                     document.getElementsByClassName('swiper_country__list')[0].style.transform = "translateY(" + this.translateY +"px)"
                     if(this.translateY < -24)
@@ -102,7 +102,7 @@
                 }
                 if(el.changedTouches[0].clientY > this.y && (el.changedTouches[0].clientY - this.y < 100))
                 {
-                    this.translateY += 1.8
+                    this.translateY += 3.6
                     document.getElementsByClassName('swiper_country__list')[0].style.transition = "transform .1s linear;"
                     document.getElementsByClassName('swiper_country__list')[0].style.transform = "translateY(" + this.translateY +"px)"
                     if(this.translateY > 24)
